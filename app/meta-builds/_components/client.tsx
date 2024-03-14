2dedede654d8df97b29dc96359d2514113eb0fd6
Alex Sherman <asherman.ca@gmail.com>
2024-03-14 08:35:45 -0700

    meta build - search field

--- app/meta-builds/_components/client.tsx
+++ app/meta-builds/_components/client.tsx
@@ -3,6 +3,7 @@ import { FC, useState } from 'react'
 import { Input } from '@/components/ui/input'
 import { cn } from '@/lib/utils'
 import Image from 'next/image'
+import { HiMagnifyingGlass } from 'react-icons/hi2'
 
 const classNames = ['Gunner', 'Scout', 'Driller', 'Engineer'] as const
 
@@ -24,14 +25,17 @@ const page: FC<pageProps> = ({ classes }) => {
 
 	return (
 		<div className='parent'>
-			<div className='gutters py-4'>
+			<div className='gutters py-4 space-y-4'>
 				<div className='flex justify-between'>
 					<h1 className='font-semibold text-xl'>DRGS Meta Builds</h1>
-					<Input
-						type='text'
-						placeholder='Search by build or skill...'
-						className='w-[250px]'
-					/>
+					<div className='p-2 flex gap-2 items-center w-[250px] bg-primary/10 group rounded-md'>
+						<HiMagnifyingGlass className='h-5 w-5' />
+						<Input
+							type='text'
+							placeholder='Search by build or skill...'
+							className='w-full bg-transparent group-focus-within:ring-0 group-focus-within:outline-none text-primary p-0 h-fit'
+						/>
+					</div>
 				</div>
 
 				<div className='flex gap-4'>
