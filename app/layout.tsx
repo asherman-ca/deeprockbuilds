cba18de2c7d3450cf984a0d517242be8f522ad61
Alex Sherman <asherman.ca@gmail.com>
2024-03-26 16:27:23 -0700

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { SessionProvider } from 'next-auth/react'
+import { Analytics } from '@vercel/analytics/react'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
@@ -28,6 +29,7 @@ export default async function RootLayout({
 				>
 					<Navbar />
 					{children}
+					<Analytics />
 					<Footer />
 				</body>
 			</html>
