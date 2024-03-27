56d8d56bc33990ddc1e58666a1fb3a97d325562d
Alex Sherman <asherman.ca@gmail.com>
2024-03-26 21:46:41 -0700

    loading - ssr loading components

---
+++ app/(protected)/build-planner/loading.tsx
@@ -0,0 +1,10 @@
+import LoadingTile from '@/components/LoadingTile'
+import { FC } from 'react'
+
+interface loadingProps {}
+
+const loading: FC<loadingProps> = ({}) => {
+	return <LoadingTile />
+}
+
+export default loading
